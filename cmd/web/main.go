cad81a0e00e2fd7f100e91086728e2d7bbf3fc07
Akbar Goldust <agoldust@gmail.com>
2023-03-28 13:39:00 +0330

    adding base layout

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,14 +3,16 @@ package main
 import (
 	"log"
 	"net/http"
+
+	handler "github.com/agoldust/go-course/pkg/handlers"
 )
 
 const portNumber = ":8080"
 
 // main main function of app
 func main() {
-	http.HandleFunc("/", Home)
-	http.HandleFunc("/about", About)
+	http.HandleFunc("/", handler.Home)
+	http.HandleFunc("/about", handler.About)
 
 	log.Println("Starting Application on http://localhost:8080")
 	_ = http.ListenAndServe(portNumber, nil)
